ab7d72a2e9681ec42ee22c0a7c5d8275a42410d2
Phinehas Enakireru <phinehasenakireru@gmail.com>
2022-10-18 16:42:12 +0100

    initial commit - add ProgramEntity

---
+++ program-service/src/main/java/dev/bigdecimal/yorpat/api/programservice/ProgramEntity.java
@@ -0,0 +1,25 @@
+
+package dev.bigdecimal.yorpat.api.programservice;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "Program")
+@Data
+public class ProgramEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long programId;
+    private String programName;
+    @Column(nullable = false)
+    private Date programDate;
+}
---
+++ program-service/src/test/java/dev/bigdecimal/yorpat/api/programservice/ProgramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bigdecimal.yorpat.api.programservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
